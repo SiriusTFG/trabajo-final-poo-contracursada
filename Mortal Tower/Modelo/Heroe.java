6b585ead8c63245ed900d997a18e537a2aafd5f6
Davidelcheff456 <caseres.marcosdavid@gmail.com>
2026-04-17 14:13:31 -0300

    de manera basica se crea la clase heroe

---
+++ Mortal Tower/Modelo/Heroe.java
@@ -0,0 +1,12 @@
+package Modelo;
+
+public class Heroe extends Entidad {
+
+    public Heroe(String nombre, int vida, int mana) {
+        super(nombre, vida, mana);
+    }
+@Override
+public void realizarTurno (Entidad objetivo){
+    
+}
+}
