096fda9c1ce0eeb22fcd4a7066cf25d0348553f2
Davidelcheff456 <caseres.marcosdavid@gmail.com>
2026-05-23 21:59:36 -0300

    Se implementa la eliminacion de partidas guardadas

--- core/src/main/java/com/mortaTower/Screens/CargarScreen.java
+++ core/src/main/java/com/mortaTower/Screens/CargarScreen.java
@@ -3,8 +3,6 @@ package com.mortaTower.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.mortaTower.Controlador.CargarControlador;
-import com.mortaTower.Controlador.CargarControlador.Action;
 import com.mortaTower.DAO.PartidaDao;
 import com.mortaTower.Main;
 import com.mortaTower.Modelo.CargarModelo;
@@ -14,83 +12,151 @@ import com.mortaTower.Vista.CargarVista;
 public class CargarScreen extends Screens {
 
     private final CargarModelo modelo;
-    private final CargarControlador controlador;
     private final CargarVista vista;
     private final Screens anteriorScreen;
+    private int indicePartidaAEliminar = -1;
 
-    public CargarScreen(Main game, Screens anteriorScreen){
-
+    public CargarScreen(Main game, Screens anteriorScreen) {
         super(game);
+
         this.anteriorScreen = anteriorScreen;
         this.modelo = new CargarModelo();
-        this.controlador = new CargarControlador(modelo, game.teclado, game.audio);
         this.vista = new CargarVista(modelo, viewport, game);
+
         Gdx.input.setInputProcessor(vista.getStage());
+
+        configurarBotonesPartidas();
+        configurarBotonesBorrar();
+        configurarBotonesModal();
+    }
+
+    private void configurarBotonesPartidas() {
         for (int i = 0; i < vista.getBotonesPartidas().size(); i++) {
             final int index = i;
+
             vista.getBotonesPartidas().get(i).addListener(new ClickListener() {
                 @Override
-            public void enter(InputEvent event, float x, float y, int pointer, com.badlogic.gdx.scenes.scene2d.Actor fromActor) {
-                super.enter(event, x, y, pointer, fromActor);
-                game.audio.play(1);
-            }
-            public void clicked(InputEvent event, float x, float y) {
-                game.audio.play(2);
-            modelo.setSeleccion(index);
-            cargarPartidaSeleccionada();
+                public void enter(InputEvent event, float x, float y, int pointer,
+                                  com.badlogic.gdx.scenes.scene2d.Actor fromActor) {
+                    game.audio.play(1);
+                }
+
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    game.audio.play(2);
+                    modelo.setSeleccion(index);
+                    cargarPartidaSeleccionada();
                 }
             });
         }
     }
-   
+
+    private void configurarBotonesBorrar() {
+        for (int i = 0; i < vista.getBotonesBorrar().size(); i++) {
+            final int index = i;
+
+            vista.getBotonesBorrar().get(i).addListener(new ClickListener() {
+                @Override
+                public void enter(InputEvent event, float x, float y, int pointer,
+                                  com.badlogic.gdx.scenes.scene2d.Actor fromActor) {
+                    game.audio.play(1);
+                }
+
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    game.audio.play(2);
+
+                    indicePartidaAEliminar = index;
+
+                    String resumen = modelo.getPartidas().get(index);
+                    String[] partes = resumen.split(" - ", 2);
+                    String textoVisible = (partes.length > 1) ? partes[1] : resumen;
+
+                    vista.mostrarModalEliminar(textoVisible);
+                }
+            });
+        }
+    }
+
+    private void configurarBotonesModal() {
+        vista.getBtnCancelarEliminar().addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.audio.play(1);
+                indicePartidaAEliminar = -1;
+                vista.ocultarModalEliminar();
+            }
+        });
+
+        vista.getBtnConfirmarEliminar().addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.audio.play(2);
+
+                if (indicePartidaAEliminar >= 0) {
+                    eliminarPartidaSeleccionada();
+                }
+            }
+        });
+    }
 
     private void cargarPartidaSeleccionada() {
-    int seleccion = modelo.getSeleccion();
+        int seleccion = modelo.getSeleccion();
 
-    String resume = modelo.getPartidas().get(seleccion);
-    int idPartida = Integer.parseInt(resume.split(" - ")[0]);
+        String resumen = modelo.getPartidas().get(seleccion);
+        int idPartida = Integer.parseInt(resumen.split(" - ")[0]);
 
-    try {
-        PartidaDao partidaDao = new PartidaDao();
-        Partida partida = partidaDao.cargarPartida(idPartida);
+        try {
+            PartidaDao partidaDao = new PartidaDao();
+            Partida partida = partidaDao.cargarPartida(idPartida);
 
-        if (partida != null) {
-            game.setPartida(partida);
-            game.setScreen(new TransicionScreen(game, this, new CombateScreen(game)));
+            if (partida != null) {
+                game.setPartida(partida);
+                game.setScreen(new TransicionScreen(game, this, new CombateScreen(game)));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al cargar la partida: " + e.getMessage());
         }
-
-    } catch (Exception e) {
-        e.printStackTrace();
-        System.out.println("Error al cargar la partida: " + e.getMessage());
-    }
     }
 
+    private void eliminarPartidaSeleccionada() {
+        String resumen = modelo.getPartidas().get(indicePartidaAEliminar);
+        int idPartida = Integer.parseInt(resumen.split(" - ")[0]);
+
+        try {
+            PartidaDao partidaDao = new PartidaDao();
+            partidaDao.borrarPartida(idPartida);
+
+            game.setScreen(new CargarScreen(game, anteriorScreen));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al borrar la partida: " + e.getMessage());
+        }
+    }
+
+    @Override
     public void update(float delta) {
-
-        Action action = controlador.update();
-
-        switch (action) {
-
-                case CONFIRM_LOAD -> {cargarPartidaSeleccionada();}
-                
-                case CANCEL -> { game.setScreen(new TransicionScreen(game, this, anteriorScreen));
-            }
-                case NONE -> {}
-            }
-
+        if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.ESCAPE)) {
+            game.audio.play(1);
+            game.setScreen(new TransicionScreen(game, this, anteriorScreen));
         }
+    }
+
     @Override
     public void render(float delta) {
         super.render(delta);
         vista.getStage().act(delta);
         vista.getStage().draw();
     }
+
+    @Override
     public void draw(float delta) {}
 
-           
     @Override
     public void dispose() {
-        vista.cerrar ();
+        vista.cerrar();
     }
-
 }
--- core/src/main/java/com/mortaTower/Vista/CargarVista.java
+++ core/src/main/java/com/mortaTower/Vista/CargarVista.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mortaTower.Main;
 import com.mortaTower.Modelo.CargarModelo;
@@ -22,12 +24,18 @@ public class CargarVista {
  private CargarModelo modelo;
  private Stage stage;
  private List<ImageButton> botonesPartidas = new ArrayList<>();
+ private List<ImageButton> botonesBorrar = new ArrayList<>();
+
+ private Group modalEliminar;
+ private Label textoPartidaEliminar;
+ private ImageButton btnCancelarEliminar;
+ private ImageButton btnConfirmarEliminar;
 
  public CargarVista(CargarModelo modelo, FitViewport viewport, Main game) {
     this.modelo = modelo;
     this.stage = new Stage(viewport, game.batch);
 
-    Texture fondo = new Texture("Imagenes/MenuInicio/Fondo1.png");
+    Texture fondo = new Texture("Imagenes/CargarPartida/Fondo1.png");
     Image imgFondo = new Image(fondo);
     imgFondo.setSize(stage.getWidth(), stage.getHeight());
     stage.addActor(imgFondo);
@@ -36,14 +44,18 @@ public class CargarVista {
     tabla.setFillParent(true);
     tabla.top().padTop(190);
 
-    Texture texNormal = new Texture("Imagenes/MenuInicio/boton.png");
-    Texture texSelected = new Texture("Imagenes/MenuInicio/boton1.png");
+    Texture texNormal = new Texture("Imagenes/CargarPartida/boton.png");
+    Texture texSelected = new Texture("Imagenes/CargarPartida/boton1.png");
+    Texture texBorrar = new Texture("Imagenes/CargarPartida/calaberaBorrar.png");
+    Texture texBorrarHover = new Texture("Imagenes/CargarPartida/calaberaBorrar1.png"); 
 
     for (int i = 0; i < modelo.getPartidas().size(); i++) {
         ImageButton boton = crearBoton (texNormal, texSelected);
+        ImageButton botonBorrar = crearBoton(texBorrar, texBorrarHover);
 
         Label.LabelStyle estilo = new Label.LabelStyle();
         estilo.font = new com.badlogic.gdx.graphics.g2d.BitmapFont();
+        textoPartidaEliminar = new Label("", estilo);
         
         String resumen = modelo.getPartidas().get(i);
 
@@ -62,15 +74,53 @@ public class CargarVista {
         stack.add(boton);
         stack.add(textoCentrado);
 
-        tabla.add(stack).width(560).height(90).padBottom(5).row();
+        Table fila = new Table();
+        fila.add(stack).width(560).height(90).padRight(-50);
+        fila.add(botonBorrar).width(56).height(56);
+
+        tabla.add(fila).padBottom(-2).row();
+
         botonesPartidas.add(boton);
+        botonesBorrar.add(botonBorrar);
 
 
     }
     stage.addActor(tabla);
+    crearModalEliminar();
     }
+    private void crearModalEliminar() {
+        modalEliminar = new Group();
+        modalEliminar.setSize(stage.getWidth(), stage.getHeight());
+        modalEliminar.setVisible(false);
 
-    private ImageButton crearBoton(Texture normal, Texture seleccionado) {
+        Image cuadro = new Image(new Texture("Imagenes/CargarPartida/CuadroEliminar.png"));
+        cuadro.setSize(620, 360);
+        cuadro.setPosition((stage.getWidth() - 620) / 2f, (stage.getHeight() - 320) / 2f);
+        modalEliminar.addActor(cuadro);
+        Label.LabelStyle estilo = new Label.LabelStyle();
+        estilo.font = new com.badlogic.gdx.graphics.g2d.BitmapFont();
+
+        
+        textoPartidaEliminar.setAlignment(Align.center);
+        textoPartidaEliminar.setWidth(500);
+        textoPartidaEliminar.setPosition((stage.getWidth() - 500) / 2f, (stage.getHeight() + 95) / 2f - 15); 
+        modalEliminar.addActor(textoPartidaEliminar);
+        btnCancelarEliminar = crearBoton( new Texture("Imagenes/CargarPartida/cancelarBoton.png"), new Texture("Imagenes/CargarPartida/cancelarBoton1.png"));
+
+        btnConfirmarEliminar = crearBoton(new Texture("Imagenes/CargarPartida/eliminarBoton.png"), new Texture("Imagenes/CargarPartida/eliminarBoton1.png"));
+        
+        btnCancelarEliminar.setSize(190, 55);
+        btnConfirmarEliminar.setSize(190, 55);
+
+        btnCancelarEliminar.setPosition(stage.getWidth() / 2f - 220, stage.getHeight() / 2f - 85);
+        btnConfirmarEliminar.setPosition(stage.getWidth() / 2f + 30, stage.getHeight() / 2f - 85);
+
+        modalEliminar.addActor(btnCancelarEliminar);
+        modalEliminar.addActor(btnConfirmarEliminar);
+
+    stage.addActor(modalEliminar);
+}
+        private ImageButton crearBoton(Texture normal, Texture seleccionado) {
         ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
 
         style.imageUp = new TextureRegionDrawable(new TextureRegion(normal));
@@ -84,6 +134,25 @@ public class CargarVista {
 
     public List<ImageButton> getBotonesPartidas() {
         return botonesPartidas;
+    }
+
+    public List<ImageButton> getBotonesBorrar() {
+        return botonesBorrar;
+    }
+    public ImageButton getBtnCancelarEliminar() {
+        return btnCancelarEliminar;
+    }
+    public ImageButton getBtnConfirmarEliminar() {
+        return btnConfirmarEliminar;
+    }
+    public void ocultarModalEliminar() {
+        modalEliminar.setVisible(false);
+    }
+
+    public void mostrarModalEliminar(String textoPartida) {
+        textoPartidaEliminar.setText(textoPartida);
+        modalEliminar.setVisible(true);
+        
     }
     public void cerrar() {
         stage.dispose();
